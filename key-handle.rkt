7028961ebc9cfc469c38f55dd94adb6e9d3c3394
ACamenzind <camenal@usi.ch>
2015-12-09 14:23:24 +0100

    We can now move diagonally fk yeah !!!!

--- key-handle.rkt
+++ key-handle.rkt
@@ -11,19 +11,7 @@
 ; Gamestate + key input -> Gamestate
 
 (define (keyhandler state key)
-  (cond [(string=? key "a")
-         (make-GameState (make-Player (Player-img (GameState-player state))
-                                      (Player-health (GameState-player state))
-                                      (make-posn (- (posn-x (Player-position (GameState-player state))) SPEED)
-                                                 (posn-y (Player-position (GameState-player state))))
-                                      (make-posn -1
-                                                 (posn-y (Player-direction (GameState-player state))))
-                                      (Player-Weapon (GameState-player state))
-                                       )
-                         (GameState-Zombies state)
-                         (GameState-Projectiles state)
-                         (GameState-Score state))]
-        
+  (cond 
         [(string=? key "d")
          (make-GameState (make-Player (Player-img (GameState-player state))
                                       (Player-health (GameState-player state))
@@ -36,6 +24,19 @@
                          (GameState-Zombies state)
                          (GameState-Projectiles state)
                          (GameState-Score state))]
+        [(string=? key "a")
+         (make-GameState (make-Player (Player-img (GameState-player state))
+                                      (Player-health (GameState-player state))
+                                      (make-posn (- (posn-x (Player-position (GameState-player state))) SPEED)
+                                                 (posn-y (Player-position (GameState-player state))))
+                                      (make-posn -1
+                                                 (posn-y (Player-direction (GameState-player state))))
+                                      (Player-Weapon (GameState-player state))
+                                       )
+                         (GameState-Zombies state)
+                         (GameState-Projectiles state)
+                         (GameState-Score state))]
+        
 
         [(string=? key "w")
          (make-GameState (make-Player (Player-img (GameState-player state))
@@ -54,7 +55,7 @@
                                       (Player-health (GameState-player state))
                                       (make-posn (posn-x (Player-position (GameState-player state)))
                                                  (+ (posn-y (Player-position (GameState-player state))) SPEED))
-                                      (make-posn (posn-y (Player-direction (GameState-player state)))
+                                      (make-posn (posn-x (Player-direction (GameState-player state)))
                                                  1)
                                       (Player-Weapon (GameState-player state)))
                          (GameState-Zombies state)
--- release-key.rkt
+++ release-key.rkt
@@ -5,13 +5,25 @@
 ; Player key -> Player
 ; 
 (define (release-update-player Player key)
-  (cond [(or (string=? key "a") (string=? key "d"))  (make-Player (Player-img Player)
+  (cond [(string=? key "a")  (make-Player (Player-img Player)
                                           (Player-health Player)
                                           (Player-position Player)
                                           (make-posn 0
                                                      (posn-y (Player-direction Player)))
                                           (Player-Weapon Player))]
-        [(or (string=? key "w") (string=? key "s")) (make-Player (Player-img Player)
+        [(string=? key "d") (make-Player (Player-img Player)
+                                          (Player-health Player)
+                                          (Player-position Player)
+                                          (make-posn 0
+                                                     (posn-y (Player-direction Player)))
+                                          (Player-Weapon Player))]
+        [(string=? key "w")  (make-Player (Player-img Player)
+                                                                 (Player-health Player)
+                                                                 (Player-position Player)
+                                                                 (make-posn (posn-x (Player-direction Player))
+                                                                            0)
+                                                                 (Player-Weapon Player))]
+        [(string=? key "s") (make-Player (Player-img Player)
                                                                  (Player-health Player)
                                                                  (Player-position Player)
                                                                  (make-posn (posn-x (Player-direction Player))
